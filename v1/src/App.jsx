4cbeca7ff38871fa400f7569394ffb0b902b1e0e
kintsugi-warrior <siddhant22496@iiitd.ac.in>
2024-08-20 16:24:09 +0530

    npm install react-router-dom

--- v1/src/App.jsx
+++ v1/src/App.jsx
@@ -1,17 +1,13 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
 
 // export default function App() {
 //   return (
@@ -20,7 +16,6 @@ export default App
 //     </h1>
 //   )
 // }
-
 
 // function App() {
 //   const [count, setCount] = useState(0)
