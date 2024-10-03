9cc61ecc374dfb07b9b00c56365a74b115c97cde
kintsugi-warrior <siddhant22496@iiitd.ac.in>
2024-10-04 04:47:59 +0530

    error fixed naming func

--- v4/src/app/About/Lab/page.tsx
+++ v4/src/app/About/Lab/page.tsx
@@ -208,10 +208,10 @@ const Card = ({
       onMouseLeave={() => setHovered(false)}
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative"
     >
-      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+      <Lcon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Lcon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Lcon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Lcon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
 
       <AnimatePresence>
         {hovered && (
@@ -269,7 +269,7 @@ const V3= () => {
   );
 };
 
-export const Icon = ({ className, ...rest }: any) => {
+const Lcon = ({ className, ...rest }: any) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
