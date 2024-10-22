b17124c384424f74991897601a81cb7cd54fde9a
kintsugi-warrior <siddhant22496@iiitd.ac.in>
2024-10-23 04:25:50 +0530

    The End ;)

--- v5/src/pagesnotbuild/About/AluminiStories/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AluminiStories = () => {
-  return (
-    <div>AluminiStories</div>
-  )
-}
-
-export default AluminiStories
--- v5/src/pagesnotbuild/Opportunities/IP/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const IP = () => {
-  return (
-    <div>IP</div>
-  )
-}
-
-export default IP
