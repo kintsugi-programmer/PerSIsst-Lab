6d2120520e775f710836158f1fb3275da38a575a
Vinay Kumar Dubey <Vinay22573@iiitd.ac.in>
2025-01-28 03:46:37 +0530

    Changed the design of Open Positions

--- v6/src/components/OpenPositions.tsx
+++ v6/src/components/OpenPositions.tsx
@@ -1,11 +1,90 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
 import { LampContainer } from "@/components/ui/lamp";
 import HowToApply from "@/components/HowToApply";
-import { AvailablePositions, Position } from '@/data/positions';
-import '@/styles/positions-card.css';
 import Link from "next/link";
+import { AvailablePositions, Position } from '../data/positions';
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import '@/styles/positions-card.css';
+const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: Position[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gap-6",
+        className
+      )}
+    >
+      {items.map((item, idx) => (
+        <div
+          
+          key={item.id}
+          className="relative group block p-2 h-full w-full"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 h-full w-full bg-purple-800/95 dark:bg-slate-800/[0.8] block rounded-3xl"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <div className="relative z-20 rounded-2xl bg-slate-950  border-2 border-slate-100 dark:border-white/[0.2] group-hover:border-slate-700 p-4">
+            <article className="flex flex-col items-center gap-10 text-white">
+              <h2 className="text-xl text-[#9961D9] px-6 font-bold text-center font-poppins tracking-wide">
+                {item.title}
+              </h2>
+              <div className="flex flex-col gap-2 text-left text-white">
+                <div>
+                  <h3 className="text-lg font-semibold tracking-wider">
+                    Description:
+                  </h3>
+                  <p className="text-sm tracking-tight">{item.description}</p>
+                </div>
+                <div>
+                  <h3 className="text-lg font-semibold tracking-wider">
+                    Qualification:
+                  </h3>
+                  <p className="text-sm tracking-tight">{item.qualification}</p>
+                </div>
+              </div>
+              <a
+                href={item.learnMoreLink}
+                className="p-[1.5px] relative mb-3 inline-block"
+              >
+                <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-fuchsia-500 rounded-2xl" />
+                <div className="text-base font-poppins font-normal px-4 py-2 bg-[#10072B] rounded-[16px] relative group transition duration-200 text-white">
+                  Apply now
+                </div>
+              </a>
+            </article>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 const JoinUs = () => {
   return (
@@ -54,37 +133,8 @@ const JoinUs = () => {
         </LampContainer>
         {/* This is the part which we might need removing also */}
         <div className=" w-full relative">
-            <div className="flex bg-slate-950/40 flex-col sm:flex-row gap-10 sm:gap-6 items-center justify-center p-2 mb-10 w-full relative z-100">
-              {AvailablePositions.map((position: Position) => (
-                <article
-                  key={position.id}
-                  className="max-sm:w-full sm:w-[280px] sm:min-w-[280px] px-8 py-5 flex flex-col items-center gap-10 text-white rounded-2xl card__article"
-                >
-                  <div className="lg:card__scale-1"></div>
-                  <div className="lg:card__scale-2"></div>
-                  <div className="relative z-[3] w-full flex flex-col gap-5 rounded-xl">
-                    <h2 className="text-xl text-[#9961D9] px-6 font-bold text-center font-poppins tracking-wide">
-                      {position.title}
-                    </h2>
-                    <div className="flex flex-col gap-2 text-left text-white">
-                      <div>
-                        <h3 className="text-lg font-semibold tracking-wider">Description:</h3>
-                        <p className="text-sm tracking-tight">{position.description}</p>
-                      </div>
-                      <div>
-                        <h3 className="text-lg font-semibold tracking-wider">Qualification:</h3>
-                        <p className="text-sm tracking-tight">{position.qualification}</p>
-                      </div>
-                    </div>
-                  </div>
-                  <a href={position.learnMoreLink} className="p-[1.5px] relative mb-3">
-                    <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-fuchsia-500 rounded-2xl" />
-                    <div className="text-base font-poppins font-normal px-4 py-2 bg-[#10072B] rounded-[16px] relative group transition duration-200 text-white">
-                      Apply now
-                    </div>
-                  </a>
-                </article>
-              ))}
+            <div className="flex bg-slate-950/40 flex-col sm:flex-row gap-10 sm:gap-6 items-center justify-centerp-2 lg:p-10 mb-10 w-full  relative z-100">
+              <HoverEffect items={AvailablePositions} />
             </div>
           </div>
           
